fb96be15b0d45c68ee1657b0e68539bbe6c45a8f
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-24 01:27:32 -0400

    added dist

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // server.js
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
