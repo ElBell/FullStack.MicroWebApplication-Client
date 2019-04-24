27a73a429df68b47b06b977e9daf5e2faa720e0b
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-23 22:02:51 -0400

    moved everything up

---
+++ src/app/components/comment-form/comment-form.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {Comments} from '../../models/Comments';
+import {ActivatedRoute} from '@angular/router';
+import {BlogApiService} from '../../services/blog.api.service';
+
+@Component({
+  selector: 'app-comment-form',
+  templateUrl: './comment-form.component.html',
+  styleUrls: ['./comment-form.component.css']
+})
+export class CommentFormComponent implements OnInit {
+  private post;
+  private showing = false;
+  private comments = [];
+  private model = new Comments(null, null, null, null, null);
+  private creator = localStorage.getItem('username');
+  constructor(private postId: ActivatedRoute, private blogApiService: BlogApiService) { }
+
+  ngOnInit() {
+   this.setPost(this.postId);
+   this.getComments(this.postId);
+   setTimeout(() => this.resetModel(), 200);
+  }
+
+  getComments(postId) {
+     this.blogApiService.getCommentsByPost(postId.params.value.id)
+      .subscribe((data: any[]) => {this.comments = data; });
+  }
+  setPost(postId) {
+    this.blogApiService.getPostById(postId.params.value.id)
+      .subscribe(data => {this.post = data });
+  }
+  onClick() {
+    this.showing = !this.showing;
+  }
+  onClickAdd() {
+    this.blogApiService.createComment(this.model);
+    this.comments.push(this.model);
+    this.resetModel();
+    this.showing = false;
+  }
+  onDelete(commentId) {
+    this.blogApiService.deleteComment(commentId);
+    this.comments = this.comments.filter( comment => comment.commentId !== commentId);
+  }
+
+  onUpdate(comment) {
+    if (comment !== this.model) {
+      this.model = comment;
+    } else {
+      this.blogApiService.updateComment(this.model);
+      this.resetModel();
+    }
+  }
+
+  resetModel() {
+    this.model = new Comments(null, '', new Date(), this.post,
+      {id: localStorage.getItem('userid'), name: localStorage.getItem('username')});
+  }
+
+}
